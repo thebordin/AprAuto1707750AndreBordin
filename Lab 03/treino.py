0f8dc5d6b0a5e5e3986ba729857a8d768fefc466
thebordin <115535431+thebordin@users.noreply.github.com>
2023-05-13 13:05:04 +0100

    Commit

--- Lab 03/treino.py
+++ Lab 03/treino.py
@@ -26,46 +26,60 @@ features, features_teste, labels, labels_teste = train_test_split(dataset.iloc[:
                                                                   random_state=rdm_state)
 
 #-MODELO-#
-trickbag = Pipeline([
-    ('preprocessor', Pipeline(steps=[
-        ('pca', PCA(n_components=2,
-                random_state=rdm_state)),
-        ('kmeans', KMeans(n_clusters= clusters,
+preprocessor = PCA(n_components=2,random_state=rdm_state)
+sheepherd = KMeans(n_clusters= clusters,
                       init='k-means++',
                       random_state=rdm_state,
                       n_init= 'auto',
-                      max_iter=max_iter)),])),
+                      max_iter=max_iter)
+
+trickbag = Pipeline([
+    ('trick', Pipeline(steps=[('pca', preprocessor),('kmeans', sheepherd)])),
     ('features_teste', features_teste),
     ('labels_teste', labels_teste),
-    ('plot_title', ('Disperssão de bolhas por KMeans\nTreino/Teste=%.0f%%/%.0f%%, Clusters=%d, Random State=%d, Max iter=%d'% (((1-proporcao_treino_teste)*100),((proporcao_treino_teste)*100),clusters,rdm_state,max_iter)))
+    ('plot_title', ('Disperssão de bolhas por KMeans\nTreino/Teste=%.0f%%/%.0f%%\nClusters=%d, Random State=%d, Max iter=%d'% (((1-proporcao_treino_teste)*100),((proporcao_treino_teste)*100),clusters,rdm_state,max_iter)))
 ])
-preprocessor = trickbag.named_steps['preprocessor']
-plot_title = trickbag.named_steps['plot_title']
+trick = trickbag['trick']
+plot_title = trickbag['plot_title']
 #-FimModelo-#
 
-#Treinamento e transformação de x em PCA
-feat_pca = preprocessor.fit_transform(features)
-#feat_pca = preprocessor.named_steps['pca'].fit_transform(features)
+#Treinamento e transformação de x com PCA (OU PCA/KMEANS)
+trick.fit(features)
+feat_pca_pipe = trick.transform(features)
+feat_pca = trick['pca'].transform(features)
+print('PIPE: \n',feat_pca_pipe[0:1],'\nSTEPS:\n', feat_pca[0:1]) #>>>>>>>>>> PORQUE O PIPELINE INTEIRO GERA RESULTADO DIFERENTE ?
+
+feat_pca_kmeans = trick['kmeans'].transform(feat_pca) #Transform X to a cluster-distance space with 10 COLUMNS
+
+print('PIPE: \n',feat_pca_pipe[0:1],'\nSTEPS:\n', feat_pca_kmeans[0:1]) #>>>>>>>>>> PORQUE ELE PASSA POR 2 TRANSFORMACOES : PCA E KMEANS !!!!!
 
 #Treinamento e criação de Clusters
-#label_pred = preprocessor.named_steps['kmeans'].fit_predict(feat_pca)
-label_pred = preprocessor.fit_predict(feat_pca)
-
+label_pred_pipe = trick.predict(features)
+label_pred = trick['kmeans'].predict(feat_pca)
 #Apresentando o Score:
-print('Score do modelo: ',preprocessor.score(feat_pca))
+print('Score do modelo PIPE: ',trick.score(features))
+print('Score do modelo KMEANS: ',trick['kmeans'].score(feat_pca))
 
 #Montando o plot e definindo os Centroids:
+plt.figure(figsize=(8,8))
+plt.subplot(221)
+plt.scatter(x=feat_pca_pipe[:,0], #<<<<<<<<<<<<<< DADOS ERRADOS.... ELE SÓ USA 2/10 COLUNAS
+            y=feat_pca_pipe[:,1],
+            c=label_pred)
+
+plt.title(plot_title +'\n!!!!!!WRONG FULL PIPE!!!!!!')
+plt.subplot(224)
 plt.scatter(x=feat_pca[:,0],
             y=feat_pca[:,1],
             c=label_pred)
-centroids = preprocessor.named_steps['kmeans'].cluster_centers_
+centroids = trick['kmeans'].cluster_centers_
 plt.scatter(centroids[:,0], centroids[:,1],
             marker='*',
             s=360,
             linewidths=1,
             color='green',
             edgecolors='black',)
-plt.title(plot_title)
+plt.title(plot_title+'\nSTEPS')
 plt.show()
 #Gravar o predictor.
 predictor_file= open(url_predictor, 'wb')
