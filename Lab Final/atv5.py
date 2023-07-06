a8c7f90c1c720824673e34c5753f4e17d256079c
thebordin <115535431+thebordin@users.noreply.github.com>
2023-07-06 10:31:56 +0100

    Teste com outro Sample: Resultado Igual

--- Lab Final/atv5.py
+++ Lab Final/atv5.py
@@ -18,5 +18,5 @@ y_predicted = trickbag['MLPC'].predict(x_ss)
 y_predicted = pd.DataFrame(y_predicted)
 
 cm = confusion_matrix(y_predicted,y_validation)
-
+print(f'Validation: \n{y_validation.T} \n Predicted: \n{y_predicted.T}')
 print('Traço = %.2f || Matriz confusão = %.2f || Score = %.2f || Precisao do MLPC = %.2f'% (cm.trace(), cm.sum(), trickbag['MLPC'].score(x_ss, y_validation), accuracy_score(y_validation, y_predicted)))
